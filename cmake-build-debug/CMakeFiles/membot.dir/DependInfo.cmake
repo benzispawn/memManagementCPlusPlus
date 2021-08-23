dccf7dbf783b956561f25affb58f850f1d6784cc
Raphael <raphael_benzi@hotmail.com>
2021-08-23 14:19:34 -0300

    first adjust

---
+++ cmake-build-debug/CMakeFiles/membot.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rbenzi/Documents/C++/Udacity_projects/CppND-Memory-Management-Chatbot/src/chatbot.cpp" "/Users/rbenzi/Documents/C++/Udacity_projects/CppND-Memory-Management-Chatbot/cmake-build-debug/CMakeFiles/membot.dir/src/chatbot.cpp.o"
+  "/Users/rbenzi/Documents/C++/Udacity_projects/CppND-Memory-Management-Chatbot/src/chatgui.cpp" "/Users/rbenzi/Documents/C++/Udacity_projects/CppND-Memory-Management-Chatbot/cmake-build-debug/CMakeFiles/membot.dir/src/chatgui.cpp.o"
+  "/Users/rbenzi/Documents/C++/Udacity_projects/CppND-Memory-Management-Chatbot/src/chatlogic.cpp" "/Users/rbenzi/Documents/C++/Udacity_projects/CppND-Memory-Management-Chatbot/cmake-build-debug/CMakeFiles/membot.dir/src/chatlogic.cpp.o"
+  "/Users/rbenzi/Documents/C++/Udacity_projects/CppND-Memory-Management-Chatbot/src/graphedge.cpp" "/Users/rbenzi/Documents/C++/Udacity_projects/CppND-Memory-Management-Chatbot/cmake-build-debug/CMakeFiles/membot.dir/src/graphedge.cpp.o"
+  "/Users/rbenzi/Documents/C++/Udacity_projects/CppND-Memory-Management-Chatbot/src/graphnode.cpp" "/Users/rbenzi/Documents/C++/Udacity_projects/CppND-Memory-Management-Chatbot/cmake-build-debug/CMakeFiles/membot.dir/src/graphnode.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WXUSINGDLL"
+  "_FILE_OFFSET_BITS=64"
+  "__WXMAC__"
+  "__WXOSX_COCOA__"
+  "__WXOSX__"
+  "wxDEBUG_LEVEL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/lib/wx/include/osx_cocoa-unicode-3.1"
+  "/usr/local/include/wx-3.1"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
